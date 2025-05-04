2647da0396fdaf98410455cbb80b4fa06064192b
Darel Masis <darrelmasis@gmail.com>
2025-05-03 21:13:59 -0600

    feat: elimina rutas de About y Booking del enrutador

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { About, Booking, Contact, Gallery, History, Home, NotFound, Packages, Services } from '../pages/Index'
+import { Contact, History, Home, NotFound, Packages, Services } from '../pages/Index'
 
 const AppRoutes = () => {
   return (
     <Router basename="/">
       <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/booking" element={<Booking />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/gallery" element={<Gallery />} />
         <Route path="/history" element={<History />} />
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/packages" element={<Packages />} />
         <Route path="/services" element={<Services />} />
         <Route path="*" element={<NotFound />} />
