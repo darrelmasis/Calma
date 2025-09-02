0fbeaf05b36e1b5f7fa2b8ee837807c0ad04d2f9
Darel Masis <darrelmasis@gmail.com>
2025-09-02 07:25:30 -0600

    feat: agregar importaciones de Privacy y Booking en el componente Index

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -4,5 +4,6 @@ import History from './screens/History'
 import Packages from './screens/Packages'
 import Contact from './screens/Contact'
 import NotFound from './screens/NotFound'
-
-export { Home, Services, Packages, Contact, NotFound, History }
+import Privacy from './screens/Privacy'
+import Booking from './screens/Booking'
+export { Home, Services, Packages, Contact, NotFound, History, Privacy, Booking }
