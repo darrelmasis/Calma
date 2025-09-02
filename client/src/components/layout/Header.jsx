d72a808ebb3028823dc73eb6c2495c8c68922f7e
Darel Masis <darrelmasis@gmail.com>
2025-09-02 07:24:49 -0600

    feat: implementar ocultación del encabezado en desplazamiento hacia abajo en dispositivos móviles

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,42 +1,32 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState, useEffect, useRef } from 'react'
+import { Navbar } from './Navbar'
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [hideHeader, setHideHeader] = useState(false)
+  const lastScrollY = useRef(0)
 
-  // Función para alternar el estado del menú
-  const toggleMenu = () => {
-    setIsOpen(!isOpen)
-  }
+  useEffect(() => {
+    if (window.innerWidth <= 768) return // Solo aplica en móviles
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+
+      if (currentScrollY > lastScrollY.current && currentScrollY > 80) {
+        setHideHeader(true) // Scroll hacia abajo
+      } else {
+        setHideHeader(false) // Scroll hacia arriba
+      }
+
+      lastScrollY.current = currentScrollY
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
-    <header className="header border-bottom d-flex flex-direction-row justify-content-center align-items-center">
-      <nav className={`container navbar d-flex flex-direction-row justify-content-center align-items-center ${isOpen ? 'active' : ''}`}>
-        <ul className="m-0 navbar-nav-list d-flex flex-direction-row justify-content-center align-items-center w-100 text-center">
-          {/* Menú izquierdo */}
-          <li className="navbar-nav-item px-3">
-            <NavLink to="/services">Servicios</NavLink>
-          </li>
-          <li className="navbar-nav-item px-3">
-            <NavLink to="/packages">Paquetes</NavLink>
-          </li>
-
-          {/* Logo centrado */}
-          <li className="navbar-nav-item px-3 d-flex justify-content-center">
-            <NavLink to="/">
-              <img src="src/assets/logo_calma.png" alt="Logo de Calma" className="logo" />
-            </NavLink>
-          </li>
-
-          {/* Menú derecho */}
-          <li className="navbar-nav-item px-3">
-            <NavLink to="/contact">Contacto</NavLink>
-          </li>
-          <li className="navbar-nav-item px-3">
-            <NavLink to="/history">Historia</NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className={`header bg-container border-bottom position-sticky ${hideHeader ? 'hide' : ''}`}>
+      <Navbar />
     </header>
   )
 }
