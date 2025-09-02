2f66b28200234c29021bd417d782ffd1a3bbf6a6
Darel Masis <darrelmasis@gmail.com>
2025-09-02 07:18:54 -0600

    feat: envuelve el componente App en LanguageProvider para soporte de internacionalización

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/styles.scss'
+import { LanguageProvider } from './i18n/LanguageContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+  </StrictMode>
 )
