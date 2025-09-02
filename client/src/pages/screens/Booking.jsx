44b6f0cd56c3d38676261fe9689517f93cf3e803
Darel Masis <darrelmasis@gmail.com>
2025-09-02 07:25:41 -0600

    feat: agregar componente Footer en la pantalla de Booking

--- client/src/pages/screens/Booking.jsx
+++ client/src/pages/screens/Booking.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { usePageTitle } from '@utils/utils'
 import Header from '../../components/layout/Header'
-
+import Footer from '../../components/layout/Footer'
 const Booking = () => {
   usePageTitle('Citas')
   return (
@@ -10,6 +10,8 @@ const Booking = () => {
       <h1>Agenda tu cita</h1>
       <p>Descubre la experiencia de belleza y relajación que mereces.</p>
       <button onClick={() => alert('Próximamente podrás reservar tus citas aquí!')}>Reserva tu cita</button>
+
+      <Footer></Footer>
     </div>
   )
 }
