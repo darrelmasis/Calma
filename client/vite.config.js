3fb62f3a6afc1c903d071b319c0db7f9837d8306
Darel Masis <darrelmasis@gmail.com>
2025-09-06 14:16:11 -0600

    feat: agrega vite-plugin-svgr para soporte SVG en el proyecto con VITE

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import svgr from 'vite-plugin-svgr'
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({ svgrOptions: { icon: true } })
+  ],
   base: './',
   resolve: {
     alias: {
