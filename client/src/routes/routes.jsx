21b4813e84031a983e5739285964a0017554ab56
Darel Masis <darrelmasis@gmail.com>
2025-09-06 14:22:08 -0600

    feat: reorganiza rutas eliminando la ruta de contacto y la de privacidad

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -5,13 +5,11 @@ const AppRoutes = () => {
   return (
     <Router basename="/">
       <Routes>
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/history" element={<History />} />
         <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
         <Route path="/home" element={<Home />} />
         <Route path="/packages" element={<Packages />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/privacy" element={<Privacy />} />
         <Route path="/booking" element={<Booking />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
