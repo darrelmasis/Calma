3b603497f23bb4921885df95d23577a2cfdcd74f
Darel Masis <darrelmasis@gmail.com>
2025-09-06 14:12:53 -0600

    Agrega iconos en formato SVG para ser utilizados en la interfaz del sitio en diferentes funcionalidades

---
+++ client/src/assets/icons/src/get-icons-variants-from-regular.ps1
@@ -0,0 +1,25 @@
+# Definir rutas de las carpetas
+$src = "C:\Users\ADATOS\OneDrive - MONISA\Escritorio\DM\Proyectos\Calma\client\src\assets\icons\src\regular"
+$target = "C:\Users\ADATOS\OneDrive - MONISA\Recursos\svgs\solid"
+$dest = "C:\Users\ADATOS\OneDrive - MONISA\Escritorio\DM\Proyectos\Calma\client\src\assets\icons\src\solid"
+
+# Crear la carpeta destino si no existe
+if (-not (Test-Path -Path $dest)) {
+    New-Item -ItemType Directory -Path $dest
+}
+
+# Obtener todos los archivos de la carpeta fuente
+$srcFiles = Get-ChildItem -Path $src -File
+
+foreach ($file in $srcFiles) {
+    # Buscar el archivo con el mismo nombre en la carpeta target
+    $targetFile = Get-ChildItem -Path $target -File -Filter $file.Name
+
+    if ($targetFile) {
+        # Copiar el archivo encontrado a la carpeta destino
+        Copy-Item -Path $targetFile.FullName -Destination $dest -Force
+        Write-Output "Copiado: $($targetFile.Name)"
+    } else {
+        Write-Output "No encontrado en target: $($file.Name)"
+    }
+}
