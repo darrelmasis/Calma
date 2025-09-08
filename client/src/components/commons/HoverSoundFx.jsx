f80668e40eeb275cdfa1081e7bfb3f813b590bd3
Darel Masis <darrelmasis@gmail.com>
2025-09-07 21:59:57 -0600

    feat: agregar componente HoverSoundWrapper para reproducir sonido al hacer clic

---
+++ client/src/components/commons/HoverSoundFx.jsx
@@ -0,0 +1,37 @@
+import { Howl } from "howler";
+import { useMemo } from "react";
+import { useLocation } from "react-router-dom";
+
+const HoverSoundWrapper = ({ children, route, matchType = "except" }) => {
+  const location = useLocation();
+
+  // Creamos el sonido una sola vez
+  const hoverSound = useMemo(
+    () =>
+      new Howl({
+        src: ["/bell.ogg"],
+        volume: 0.2,
+      }),
+    []
+  );
+
+  const playSound = () => {
+    const isMatch = location.pathname === route;
+
+    if (
+      (matchType === "only" && isMatch) || // reproducir solo en la ruta específica
+      (matchType === "except" && !isMatch) // reproducir en todas menos esa ruta
+    ) {
+      hoverSound.stop();
+      hoverSound.play();
+    }
+  };
+
+  return (
+    <div onMouseDown={playSound} style={{ display: "flex", width: "100%" }}>
+      {children}
+    </div>
+  );
+};
+
+export { HoverSoundWrapper };
