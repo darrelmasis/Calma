f6cc4c53948c6ecdac16f64ebcfeae32ccde3065
Darel Masis <darrelmasis@gmail.com>
2025-09-11 00:34:05 -0600

    feat: agregar hook personalizado useOfflineStatus para detectar conexión a internet

---
+++ client/src/hooks/useOfflineStatus.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+
+/**
+ * Hook personalizado para detectar si la aplicación está offline.
+ *
+ * @returns {boolean} - true si la aplicación está offline, false si está online.
+ *
+ * ## Ejemplo de uso
+ * ```jsx
+ * const isOffline = useOfflineStatus()
+ *
+ * return (
+ *   <div>
+ *     {isOffline ? "Estás sin conexión 😢" : "Conectado ✅"}
+ *   </div>
+ * )
+ * ```
+ */
+export function useOfflineStatus() {
+  const [isOffline, setIsOffline] = useState(!navigator.onLine)
+
+  useEffect(() => {
+    const handleOffline = () => setIsOffline(true)
+    const handleOnline = () => setIsOffline(false)
+
+    window.addEventListener('offline', handleOffline)
+    window.addEventListener('online', handleOnline)
+
+
+    return () => {
+      window.removeEventListener('offline', handleOffline)
+      window.removeEventListener('online', handleOnline)
+    }
+  }, [])
+
+  return isOffline
+}
