0785802bdb0eda59529b208bbb6e38bfaf5f3e5c
Darel Masis <darrelmasis@gmail.com>
2025-09-11 22:41:42 -0600

    feat: agregar redirección de /home a la página principal y evita duplicados de rutas

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Booking, History, Home, NotFound, Packages, Services, Privacy } from '../pages/Index'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Booking, History, Home, NotFound, Packages, Services } from '../pages/Index'
 import ScrollTop from '../components/commons/ScrollTop'
 
 const AppRoutes = () => {
+
   return (
     <Router basename="/">
       <ScrollTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/history" element={<History />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/packages" element={<Packages />} />
         <Route path="/services" element={<Services />} />
         <Route path="/booking" element={<Booking />} />
