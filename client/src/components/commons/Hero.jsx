4945580cb0a746ba2f376fe8c7a6d4a957ccb26b
Darel Masis <darrelmasis@gmail.com>
2025-09-12 08:03:36 -0600

    feat: mejorar el diseño del componente Hero y optimizar estilos en _hero.scss

--- client/src/components/commons/Hero.jsx
+++ client/src/components/commons/Hero.jsx
@@ -13,31 +13,26 @@ export const Hero = () => {
 
   return (
     <section className="hero bg-container">
-      <div className="container d-flex align-items-center justify-content-flex-start flex-column flex-md-row">
+      <div className="container d-flex align-items-lg-center align-items-flex-start">
         <div className="hero__content">
-          <Fade>
+          <Fade className='d-inline-flex flex-direction-column gap-1 d-md-block align-items-center align-items-md-flex-start text-center text-md-start'>
             <h1 className="hero__title mt-0">
               {t('home.heroSection.title_1')} <span className="text-primary">{t('home.heroSection.title_2')}</span> {t('home.heroSection.title_3')}
             </h1>
-            <p className="text-muted fs-h5">
+            <p className="hero__content--description fs-h5 fs-lg-lead text-muted">
               {
                 t('home.heroSection.description')
               }
             </p>
-            <div className="grid">
-              <div className="grid-row mt-6">
-                <div className="grid-col-12 grid-col-md-5">
-                  <Button
-                    onClick={goToServices}
-                    variant="primary"
-                    ariaLabel={t('home.heroSection.button_1')}
-                    label={t('home.heroSection.button_1')}
-                    size="large"
-                    icon={{ name: "spa", size: "lg", variant: "duotones", duotone: "regular" }} />
-                </div>
-              </div>
 
-            </div>
+            <Button
+              onClick={goToServices}
+              variant="primary"
+              ariaLabel={t('home.heroSection.button_1')}
+              label={t('home.heroSection.button_1')}
+              size="large"
+              icon={{ name: "spa", size: "lg", variant: "duotones", duotone: "regular" }} />
+
           </Fade>
         </div>
       </div>
