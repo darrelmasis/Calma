5ad2aac8d3314b3694f16655a3b802f9b7cdc755
Darel Masis <darrelmasis@gmail.com>
2025-09-13 09:44:23 -0600

    feat: simplificar la configuración de manualChunks en vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -192,15 +192,12 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendor';
-            }
             if (id.includes('lodash') || id.includes('date-fns')) {
               return 'utils-vendor';
             }
             return 'vendor';
           }
-        },
+        }
       },
     },
   },
