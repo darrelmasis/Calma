574dd08e11f0ee3895c96b24920597d6052ef077
Darel Masis <darrelmasis@gmail.com>
2025-09-16 19:17:06 -0600

    feat: añadir el componente Hero2 en Home, mejorando la sección principal con nuevos estilos y contenido interactivo

--- client/src/pages/screens/Home.jsx
+++ client/src/pages/screens/Home.jsx
@@ -8,7 +8,7 @@ import { FadeInWhenVisible as Fade } from '../../components/commons/animations/F
 import Footer from '../../components/layout/Footer'
 import { useLang } from '../../i18n/LanguageContext'
 import { ResponsiveImage } from '../../components/ui/ResponsiveImage'
-
+import { Hero2 } from '../../components/commons/Hero-2'
 
 const Home = () => {
   const { t } = useLang()
@@ -22,14 +22,33 @@ const Home = () => {
   return (
     <>
       <Header />
-      <Hero />
-      <section id="why" className="py-5 bg-body d-flex align-items-center">
+      <Hero2
+        background="/images/webp/hero-bg-alt-1200.webp"
+        overlayType="end-transparent"
+        title={t('home.heroSection.title_1', { parse: true })}
+        subtitle={t('home.heroSection.description')}
+        cta={
+          [
+            {
+              onClick: goToServices,
+              variant: "primary",
+              ariaLabel: t('home.heroSection.button_1'),
+              label: t('home.heroSection.button_1'),
+              size: "xlarge",
+              icon: { name: "spa", size: "lg", variant: "duotones", duotone: "regular" }
+            }
+          ]
+        }
+        alignContent='start'
+      />
+      {/* <Hero /> */}
+      <section id="why" className="py-5 bg-neutral-0 d-flex align-items-center border-top">
         <div className="container d-flex flex-direction-column align-items-center justify-content-center">
 
           <Fade>
             <h2 className="text-primary mb-1 text-center">{t('home.section_2.title')}</h2>
           </Fade>
-          <Fade>
+          <Fade delay={0.2}>
             <p className="fs-lead text-muted mt-1 text-center">{t('home.section_2.description')}</p>
           </Fade>
 
@@ -38,18 +57,12 @@ const Home = () => {
 
             <div className="grid-col-12 grid-col-md-6 grid-col-lg-4 text-center my-lg-0 mb-3 py-3 py-lg-0">
               <div className="card d-flex flex-direction-column align-items-center">
-                <Fade>
+                <Fade delay={0.4}>
                   <span className="border-primary card-icon p-3 rounded-all-full d-inline-flex align-items-center justify-content-center">
                     <Icon name="heart" variant='regular' size="lg" className="text-primary" />
                   </span>
-                </Fade>
-                <Fade>
                   <h3 className='card-title-hack fs-h4'>{t('home.section_2.points.p1.title')}</h3>
-                </Fade>
-                <Fade>
                   <p className="text-muted wx-lg-350">{t('home.section_2.points.p1.description')}</p>
-                </Fade>
-                <Fade>
                   <ResponsiveImage name="image-01" alt="Masaje terapéutico en la espalda con una piedra calentada de forma ergonómica" className="rounded wx-xl-350" />
                 </Fade>
               </div>
@@ -57,18 +70,12 @@ const Home = () => {
 
             <div className="grid-col-12 grid-col-md-6 grid-col-lg-4 text-center my-lg-0 mb-3 py-3 py-lg-0">
               <div className="card d-flex flex-direction-column align-items-center">
-                <Fade>
+                <Fade delay={0.6}>
                   <span className="border-primary card-icon p-3 rounded-all-full d-inline-flex align-items-center justify-content-center">
                     <Icon name="shield" variant='regular' size="lg" className="text-primary" />
                   </span>
-                </Fade>
-                <Fade>
                   <h3 className='card-title-hack fs-h4'>{t('home.section_2.points.p2.title')}</h3>
-                </Fade>
-                <Fade>
                   <p className="text-muted wx-lg-350">{t('home.section_2.points.p2.description')}</p>
-                </Fade>
-                <Fade>
                   <ResponsiveImage name="image-02" alt="Aplicación de cera depilatoria en el área del labio superior con un palito de madera" className="rounded wx-xl-350" />
                 </Fade>
               </div>
@@ -76,18 +83,12 @@ const Home = () => {
 
             <div className="grid-col-12 grid-col-md-12 grid-col-lg-4 text-center my-lg-0 mb-3 py-3 py-lg-0">
               <div className="card d-flex flex-direction-column align-items-center">
-                <Fade>
+                <Fade delay={0.8}>
                   <span className="border-primary card-icon p-3 rounded-all-full d-inline-flex align-items-center justify-content-center">
                     <Icon name="users" variant='regular' size="lg" className="text-primary" />
                   </span>
-                </Fade>
-                <Fade>
                   <h3 className='card-title-hack fs-h4'>{t('home.section_2.points.p3.title')}</h3>
-                </Fade>
-                <Fade>
                   <p className="text-muted wx-lg-350">{t('home.section_2.points.p3.description')}</p>
-                </Fade>
-                <Fade>
                   <ResponsiveImage name="image-03" alt="Aplicación de cera depilatoria en el área del labio superior con un palito de madera" className="rounded wx-xl-350" />
                 </Fade>
               </div>
@@ -104,13 +105,13 @@ const Home = () => {
           <Fade>
             <Icon name="spa" size="lg" variant="duotones" duotone='regular' className="text-secondary mb-3 fs-h1" />
           </Fade>
-          <Fade>
+          <Fade delay={0.2}>
             <h2 className="mb-3 text-secondary">{t('home.section_3.title')}</h2>
           </Fade>
-          <Fade>
+          <Fade delay={0.4}>
             <p className="mb-6 text-secondary">{t('home.section_3.description')}</p>
           </Fade>
-          <Fade>
+          <Fade delay={0.6}>
             <Button
               onClick={goToServices}
               variant="secondary"
