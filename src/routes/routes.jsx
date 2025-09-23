4e8963d4758b373511d9b85ad305beab4a484021
Darel Masis <darrelmasis@gmail.com>
2025-09-23 06:46:48 -0600

    feat: agrega rutas para los componentes Empty y Success

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { Booking, Story, Home, NotFound, Packages, Services, Team } from '../pages/Index'
+import { Booking, Story, Home, NotFound, Packages, Services, Team, Empty, Success} from '../pages/Index'
 import ScrollTop from '../components/commons/ScrollTop'
 
 const AppRoutes = () => {
@@ -15,6 +15,8 @@ const AppRoutes = () => {
         <Route path="/services" element={<Services />} />
         <Route path="/booking" element={<Booking />} />
         <Route path="/team" element={<Team />} />
+        <Route path="/empty" element={<Empty />} />
+        <Route path="/success" element={<Success />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
