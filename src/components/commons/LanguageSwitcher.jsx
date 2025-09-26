3987de4d4ab5d5001a6be8ce1086bf9bead96714
Darel Masis <darrelmasis@gmail.com>
2025-09-26 07:54:08 -0600

    feat: agregar lógica de detección de dispositivo y ajustar estilos en el componente LanguageSwitcher

--- src/components/commons/LanguageSwitcher.jsx
+++ src/components/commons/LanguageSwitcher.jsx
@@ -3,6 +3,7 @@ import { useLang } from '../../i18n/LanguageContext'
 import { Button } from '../ui/Button'
 import classNames from 'classnames'
 import { Dropdown, DropdownContent, DropdownTrigger } from '../ui/Dropdown'
+import { useDevice } from '../../hooks/useBreakpoint'
 
 export const LanguageSwitcher = ({ className = '' }) => {
   const options = [
@@ -10,6 +11,10 @@ export const LanguageSwitcher = ({ className = '' }) => {
     { value: 'en', label: 'English' }
   ]
   const { lang, changeLanguage } = useLang()
+  const { type } = useDevice()
+  const isMobile = type === 'mobile'
+  const isTablet = type === 'tablet'
+  const isDesktop = type === 'desktop'
 
   const selectedOption = options.find((opt) => opt.value === lang)
 
@@ -20,10 +25,15 @@ export const LanguageSwitcher = ({ className = '' }) => {
 
   return (
     <div className={classes}>
-      <Dropdown>
-        <DropdownTrigger>
+      <Dropdown
+        position={isMobile ? 'bottom-center' : 'bottom-start'}
+        offsetX={0}
+      >
+        <DropdownTrigger className='w-100 w-lg-auto'>
           <Button
             label={selectedOption.label}
+            size={isMobile ? 'xlarge' : 'medium'}
+            fullWidth={true}
             icon={[
               {
                 name: 'language-alt',
@@ -44,50 +54,49 @@ export const LanguageSwitcher = ({ className = '' }) => {
           />
         </DropdownTrigger>
         <DropdownContent>
-          <div className='d-flex'>
-            <ul className='list-unstyled m-0 p-3 d-flex'>
-              {options.map((opt) => {
-                const flagIconName = opt.value === 'es' ? 'nicaragua' : 'eeuu'
-                const isActive = opt.value === lang
+          <ul className='list-unstyled m-0 p-3 d-flex'>
+            {options.map((opt) => {
+              const flagIconName = opt.value === 'es' ? 'nicaragua' : 'eeuu'
+              const isActive = opt.value === lang
 
-                // Definimos los íconos dinámicamente
-                const icons = [
-                  {
-                    name: flagIconName,
-                    variant: 'flags',
-                    position: 'left'
-                  }
-                ]
-
-                if (isActive) {
-                  icons.push({
-                    name: 'check',
-                    position: 'right',
-                    className: 'text-success'
-                  })
+              // Definimos los íconos dinámicamente
+              const icons = [
+                {
+                  name: flagIconName,
+                  variant: 'flags',
+                  position: 'left'
                 }
+              ]
 
-                return (
-                  <li key={opt.value} className='mb-0'>
-                    <Button
-                      className={classNames(
-                        'language-switcher-option border-0 justify-content-flex-start',
-                        {
-                          active: isActive
-                        }
-                      )}
-                      onClick={() => changeLanguage(opt.value)}
-                      variant='basic'
-                      icon={icons}
-                      ghost={true}
-                      fullWidth={true}
-                      label={opt.label}
-                    />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+              if (isActive) {
+                icons.push({
+                  name: 'check',
+                  position: 'right',
+                  className: 'text-success'
+                })
+              }
+
+              return (
+                <li key={opt.value} className='mb-0'>
+                  <Button
+                    className={classNames(
+                      'language-switcher-option border-0 justify-content-flex-start',
+                      {
+                        active: isActive
+                      }
+                    )}
+                    onClick={() => changeLanguage(opt.value)}
+                    variant='basic'
+                    icon={icons}
+                    size={isMobile ? 'xlarge' : 'medium'}
+                    ghost={true}
+                    fullWidth={true}
+                    label={opt.label}
+                  />
+                </li>
+              )
+            })}
+          </ul>
         </DropdownContent>
       </Dropdown>
     </div>
