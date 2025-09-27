5959ebda32eb61cde101497b9421e8b79f55db58
Darel Masis <darrelmasis@gmail.com>
2025-09-27 12:11:02 -0600

    feat: refactor componente Success para usar FadeInWhenVisible y mejorar la estructura del código

--- src/pages/screens/Success.jsx
+++ src/pages/screens/Success.jsx
@@ -1,38 +1,26 @@
 import { usePageTitle } from '@utils/utils'
-import Header from '../../components/layout/Header'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { Button } from '../../components/ui/Button'
 import { useEffect } from 'react'
 import { useSelectedServices } from '../../hooks/useSelectedService'
 import { useLang } from '../../i18n/LanguageContext'
 import { ConfettiExplosion } from '../../components/commons/Confetti'
 import { useState } from 'react'
+import { FadeInWhenVisible as Fade } from '../../components/commons/animations/FadeInWhenVisible'
 
 const Success = () => {
   const { t } = useLang()
   usePageTitle(t('booking.success.title'))
-  const navigate = useNavigate()
   const location = useLocation()
   const { clearServices, isLoaded } = useSelectedServices()
   const [showExplosion, setShowExplosion] = useState(false)
 
-  // Validar acceso
-  useEffect(() => {
-    setShowExplosion(true)
-  }, [])
-
   useEffect(() => {
     if (isLoaded) {
+      setShowExplosion(true)
       clearServices('bell') // Limpiar servicios seleccionados al cargar la página de éxito
     }
   }, [])
-
-  // Manejar botón regresar
-
-  const goHome = (e) => {
-    e.preventDefault()
-    navigate('/')
-  }
 
   useEffect(() => {
     // Disparar la explosión al montar el componente
@@ -41,7 +29,7 @@ const Success = () => {
     // Limpiar después de la duración (debe coincidir con `duration`)
     const timer = setTimeout(() => {
       setShowExplosion(false)
-    }, 3000) // 3 segundos = duración de la explosión
+    }, 4000) // 3 segundos = duración de la explosión
 
     return () => clearTimeout(timer)
   }, []) // solo una vez al montar
@@ -54,25 +42,30 @@ const Success = () => {
           onComplete={() => setShowExplosion(false)}
         />
       )}
-      <Header />
       {location.state?.success && (
-        <div className='d-flex flex-direction-column justify-content-center align-items-center text-center p-5'>
-          <span className='fs-display-2 mb-3'>🎉</span>
-          <h3 className='fs-h3 mb-2'>{t('booking.success.subtitle')}</h3>
-          <p className='text-muted mb-4 max-wx-500'>
-            {t('booking.success.message')}
-          </p>
+        <>
+          <Fade className='d-flex flex-direction-column  align-items-center text-center py-5'>
+            <span className='fs-display-2 mb-3'>🎉</span>
+            <h3 className='fs-h3 mb-2'>{t('booking.success.subtitle')}</h3>
+            <p className='text-muted mb-4 max-wx-500'>
+              {t('booking.success.message')}
+            </p>
 
-          {/* CTA principal */}
-          <Button
-            variant='success'
-            className='my-3'
-            size='large'
-            icon='left-to-bracket'
-            label={t('booking.success.buttonText')}
-            onClick={goHome}
-          />
-        </div>
+            {/* CTA principal */}
+            <Button
+              as='link'
+              to='/'
+              variant='success'
+              className='my-3'
+              size='large'
+              icon='left-to-bracket'
+              label={t('booking.success.buttonText')}
+            />
+          </Fade>
+          <div className='d-flex justify-content-center mt-4'>
+            <div className='w-100 d-block border-bottom max-wx-1000'></div>
+          </div>
+        </>
       )}
     </div>
   )
